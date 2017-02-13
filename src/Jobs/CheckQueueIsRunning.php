f25c626cda205b02d8c74c5a3b8d32f9a46452b7
Visti Kløft <hr.kloft@gmail.com>
2017-02-13 10:53:01 +0530

    rename

---
+++ src/Jobs/CheckQueueIsRunning.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Vistik\Jobs;
+
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\Jobs\Job;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\File;
+
+class CheckQueueIsRunning extends Job implements ShouldQueue
+{
+    use InteractsWithQueue, SerializesModels;
+
+    private $id;
+    /**
+     * @var string
+     */
+    private $path;
+
+    public function __construct($id, string $path, string $queue = null)
+    {
+        $this->id = $id;
+        if ($queue !== null) {
+            $this->onQueue($queue);
+        }
+        $this->path = $path;
+    }
+
+    /**
+     * Handle the command.
+     */
+    public function handle()
+    {
+        // This job is just used to check if job are getting processed
+        $file = $this->path . $this->getId();
+        File::put($file, true);
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+}

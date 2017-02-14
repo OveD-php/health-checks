8e3e2a435213d8b785ca4395ab044d56e0821f2d
Visti Kløft <hr.kloft@gmail.com>
2017-02-14 23:28:00 +0530

    correct path

--- config/health.php
+++ config/health.php
@@ -14,7 +14,7 @@ return [
         new QueueIsProcessing(),
         new PathIsWritable(storage_path()),
         new PathIsWritable(storage_path('logs')),
-        new PathIsWritable(storage_path('framework/session')),
+        new PathIsWritable(storage_path('framework/sessions')),
         new PathIsWritable(storage_path('framework/cache')),
     ],
     'route'  => [

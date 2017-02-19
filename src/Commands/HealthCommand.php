f3e4c698b5ccb1f2ccae0673f29ba6e0826b670e
Visti Kløft <hr.kloft@gmail.com>
2017-02-19 08:43:32 +0100

    dont print error in red

--- src/Commands/HealthCommand.php
+++ src/Commands/HealthCommand.php
@@ -38,7 +38,7 @@ class HealthCommand extends Command
 
         $rows = [];
         foreach ($output as $o) {
-            $rows[] = [$o['check'], $o['passed'] ? '<info>passed</info>' : '<error>failed</error>', $o['log'], "<error>" . $o['error']. "</error>"];
+            $rows[] = [$o['check'], $o['passed'] ? '<info>passed</info>' : '<error>failed</error>', $o['log'], $o['error']];
         }
         $this->table(['check', 'status', 'log', 'error'], $rows);
 

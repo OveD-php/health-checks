575bb904deda59e81a42ce18466cc06d7641d665
Josue Alexander Ibarra <josue@elninja.com>
2019-08-12 19:44:23 -0700

    Initialize health checks from class names
    Allows setting the health checks in the health.php as strings or arrays in the case we need to pass in a parameter.
    This fixes the invalid config.php that is generated when we cache the configuration.
    
    https://github.com/OveD-php/health-checks/issues/13

--- src/Utils/CheckList.php
+++ src/Utils/CheckList.php
@@ -7,5 +7,14 @@ use Vistik\Collections\TypedCollection;
 
 class CheckList extends TypedCollection
 {
+    public function __construct($checks) {
+        $cast = function ($check) {
+            if ($check instanceof HealthCheck) return $check;
+            if (is_string($check)) return new $check;
+            if (is_array($check)) return new $check[0](...array_slice($check, 1));
+        };
+        parent::__construct(array_map($cast, $checks));
+    }
+
     protected $type = HealthCheck::class;
 }
